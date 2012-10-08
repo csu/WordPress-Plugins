ffd4591afcd2bee14c58322dadde059909b55f01
Christopher Su <christophersu9@gmail.com>
2012-10-08 21:24:49 +0800

    Renamed
    Signed-off-by: Christopher Su <christophersu9@gmail.com>

--- blog-stats-shortcodes.php
+++ blog-stats-shortcodes.php
@@ -1,11 +1,11 @@
 <?php
 /*
 	Plugin Name: Blog Stats Shortcodes
-	Plugin URI: 
+	Plugin URI: http://christophersu.org/wp-blog-stats-shortcodes/
 	Description: Shortcodes for displaying post, comment, page, and word counts. Shortcodes: [postcount], [commentcount], [pagecount], and [totalwordcount].
 	Version: 1.0
 	Author: Christopher Su
-	Author URI: 
+	Author URI: http://christophersu.org/
 	License: GPL2
 
 	Copyright 2012  Christopher J. Su  (email : christophersu9 (at) gmail.com)
@@ -29,18 +29,21 @@ function post_count_func( $atts ) {
 	return $total;
 }
 add_shortcode( 'postcount', 'post_count_func' );
+add_shortcode( 'post-count', 'post_count_func' );
 
 function comment_count_func ( $atts ) {
 	$total = wp_count_comments()->approved;
 	return $total;
 }
 add_shortcode( 'commentcount', 'comment_count_func' );
+add_shortcode( 'comment-count', 'comment_count_func' );
 
 function page_count_func ( $atts ) {
 	$total = wp_count_posts('page')->publish;
 	return $total;
 }
 add_shortcode( 'pagecount', 'page_count_func' );
+add_shortcode( 'page-count', 'page_count_func' );
 
 function word_count_func() {
     global $wpdb;
@@ -64,6 +67,7 @@ function word_count_func() {
 }
 add_shortcode( 'totalwordcount', 'word_count_func' );
 add_shortcode( 'total-word-count', 'word_count_func' );
-add_shortcode( 'total_word_count', 'word_count_func' );
+add_shortcode( 'blogwordcount', 'word_count_func' );
+add_shortcode( 'blog-word-count', 'word_count_func' );
 
 ?>
